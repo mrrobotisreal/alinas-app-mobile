8576b27ff0615bdf8d30b25d69ca8184114405dd
Mitchell Wintrow <90mitchwintrow@gmail.com>
2024-03-02 00:02:11 -0800

    swap url with env variable

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,3 +1,4 @@
 export const emulatorURL: string = "http://10.0.2.2:9090";
 
-export const serverURL: string = "http://3.76.7.134:9090";
+export const serverURL: string =
+  process.env.EXPO_PUBLIC_SERVER_URL || "http://localhost:9090";
